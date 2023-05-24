d3ebe7b397a0a2ff37b017d0acb67771fc4b9150
tranduykhuong <tranduykhuongit@gmail.com>
2023-05-24 16:25:19 +0700

    init project FE

---
+++ Frontend/user/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import BaseHeader from './components/BaseHeader/BaseHeader.vue';
+import BaseFooter from './components/BaseFooter/BaseFooter.vue';
+</script>
+
+<template>
+  <header>
+    <BaseHeader />
+  </header>
+
+  <RouterView />
+
+  <footer>
+    <BaseFooter />
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ Frontend/user/src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomePage from '@/pages/Home/HomePage.vue';
+const AboutPage = () => import('@/pages/About/AboutPage.vue');
+const NotFound = () => import('@/pages/NotFound/NotFound.vue');
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage
+  },
+  {
+    path: '/home',
+    redirect: { path: '/' }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutPage
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: NotFound
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+});
+
+export default router;
