9975d7a310fa007478f50089daf533f0cfa3a3a4
Andy Koi <anhkhoi1192002@gmail.com>
2023-06-16 11:49:30 +0700

    Khoi: Base UI NewsPage

--- Frontend/user/src/router/index.ts
+++ Frontend/user/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '@/pages/Home/HomePage.vue';
 const AboutPage = () => import('@/pages/About/AboutPage.vue');
+const NewsPage = () => import('@/pages/News/NewsPage.vue');
 const NotFound = () => import('@/pages/NotFound/NotFound.vue');
 
 const routes = [
@@ -18,6 +19,11 @@ const routes = [
     path: '/gioithieu',
     name: 'gioithieu',
     component: AboutPage
+  },
+  {
+    path: '/tintuc',
+    name: 'tintuc',
+    component: NewsPage
   },
   {
     path: '/:catchAll(.*)*',
