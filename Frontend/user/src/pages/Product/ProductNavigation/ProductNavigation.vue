78c657dc37687109aea2e6aa2c6c011cc2cc675a
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2023-06-17 22:21:21 +0700

    TV: complete UI product

--- Frontend/user/src/pages/Product/ProductNavigation/ProductNavigation.vue
+++ Frontend/user/src/pages/Product/ProductNavigation/ProductNavigation.vue
@@ -1,6 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeVector from '@/assets/imgs/Product/HomeNavigation.svg';
+import NavIcon from '@/assets/imgs/Product/VectorNav.svg';
+</script>
 <template>
-  <div>dgdgdf</div>
+  <div>
+    <div :class="$style['product__navigation--container']">
+      <img :class="$style['product__navigation--icon-home']" :src="HomeVector" alt="Home" />
+      <p :class="$style['product__navigation--nav']">Trang chủ&nbsp;</p>
+      <img :class="$style['product__navigation--nav-icon']" :src="NavIcon" alt="nav" />
+      <p :class="$style['product__navigation--nav']">&nbsp;Dụng cụ nha khoa</p>
+    </div>
+  </div>
 </template>
 
 <style module scoped lang="scss">
