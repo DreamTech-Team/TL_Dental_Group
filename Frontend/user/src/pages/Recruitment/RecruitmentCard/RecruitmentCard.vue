ede6c982864d1b534cff95ee87d3260582c3bafb
Bao <20127557@student.hcmus.edu.vn>
2023-06-17 22:08:18 +0700

    BL: update RecruitmentPage

--- Frontend/user/src/pages/Recruitment/RecruitmentCard/RecruitmentCard.vue
+++ Frontend/user/src/pages/Recruitment/RecruitmentCard/RecruitmentCard.vue
@@ -1,24 +1,52 @@
 <script setup lang="ts">
-import { items } from './RecruitmentHandle';
+const props = defineProps({
+  items: Object,
+  style: String
+});
 </script>
 
 <template>
-  <div :class="$style['container__poster-block-card-item']" v-for="item in items" :key="item.title">
-    <div :class="$style['container__poster-block-card-item-icon']" v-if="item.icon.link !== ''">
+  <div
+    :class="$style[`container__card-${style}`]"
+    v-for="item in props.items"
+    :key="item.title.content"
+  >
+    <div :class="$style['container__card-icon']" v-if="item.icon !== ''">
       <div
         :class="[
-          $style['container__poster-block-card-item-icon-block'],
-          $style[`${item.icon.color}`]
+          $style['container__card-icon-block'],
+          $style[`container__card-icon-block-${item.icon.style}`],
+          $style[`${item.icon.style}`]
         ]"
       >
         <img :src="`${item.icon.link}`" alt="none" />
       </div>
     </div>
-    <div :class="$style['container__poster-block-card-item-title']">
-      <h3>{{ item.title }}</h3>
+    <div class="">
+      <div :class="$style[`container__card-title-${item.title.style}`]">
+        <h3>{{ item.title.content }}</h3>
+      </div>
+      <div>
+        <div
+          :class="$style[`container__card-content-${item.title.style}`]"
+          v-if="item.content.content !== ''"
+        >
+          {{ item.content.content }}
+        </div>
+        <div
+          :class="$style[`container__card-content-${item.title.style}`]"
+          v-else-if="item.content.style !== ''"
+        >
+          <ul>
+            <li><span>Thứ 2 - 6 :</span> <span>08:00 - 17:00</span></li>
+            <li><span>Thứ 2 - 6 :</span> <span>08:00 - 17:00</span></li>
+            <li><span>Chủ Nhật :</span> <span>Nghỉ</span></li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div :class="$style['container__poster-block-card-item-content']">
-      <p v-html="item.content"></p>
+    <div :class="$style['container__card-image']" v-if="item.image.link !== ''">
+      <img :src="item.image.link" alt="none" />
     </div>
   </div>
 </template>
---
+++ Frontend/user/src/pages/Recruitment/RecruitmentCardWork/RecruitmentCardWork.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import RecruitmentCard from '.././RecruitmentCard/RecruitmentCard.vue';
+import { ic_bag, ic_hourglass, ic_location } from '@/assets/imgs/Recruitment/RecruitmentImgs';
+const props = defineProps({
+  infor: Object
+});
+
+// const locationItem = [
+//   {
+//     icon: { link: ic_location, style: 'type6' },
+//     title: { content: props.infor.location, style: 'type6' },
+//     content: { content: '', style: '' },
+//     image: { link: '', style: '' }
+//   }
+// ];
+
+// const contentRecruit = [
+//   {
+//     icon: { link: ic_hourglass, style: 'type6' },
+//     title: { content: '', style: 'type6' },
+//     content: { content: '', style: '' },
+//     image: { link: '', style: '' }
+//   },
+//   {
+//     icon: { link: ic_bag, style: 'type6' },
+//     title: { content: '', style: 'type6' },
+//     content: { content: '', style: '' },
+//     image: { link: '', style: '' }
+//   }
+// ];
+</script>
+
+<template>
+  <div>
+    <div class="">
+      <div class="">VHG – Tuyển Thực Tập Sinh Marketing</div>
+      <div class="">
+        <recruitment-card
+          :items="[
+            {
+              icon: { link: ic_location, style: 'type6' },
+              title: { content: props.infor.location, style: 'type6' },
+              content: { content: '', style: '' },
+              image: { link: '', style: '' }
+            }
+          ]"
+          :style="'type6'"
+        />
+      </div>
+    </div>
+    <div class="">
+      <div class=""><recruitment-card :items="locationItem" :style="'type6'" /></div>
+      <div class="">
+        <p>Ứng tuyển ngay</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style module scoped lang="scss">
+@import './RecruitmentCardWork.module.scss';
+</style>
