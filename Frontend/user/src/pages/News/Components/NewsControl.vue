5eca6234af2dbe84bf80cc1705c89ef8102d4fec
Andy Koi <anhkhoi1192002@gmail.com>
2023-06-18 23:20:35 +0700

    Khoi: Tags News

--- Frontend/user/src/pages/News/Components/NewsControl.vue
+++ Frontend/user/src/pages/News/Components/NewsControl.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { faSortDown, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { ref } from 'vue';
 
-let isCustomSelectListOpen = false;
+//Open and Close Modal
+const isCustomSelectListOpen = ref(false);
+
+//Array Available
+const availableItems = ref<string[]>(['Hoạt động', 'Tin tức', 'Tư vấn', 'Hội thảo']);
+const selectedItems = ref<string[]>([]);
 
 const clickHandle = () => {
-  isCustomSelectListOpen = !isCustomSelectListOpen;
-  console.log('customSelectListDisplay');
+  isCustomSelectListOpen.value = !isCustomSelectListOpen.value;
 };
 
-const closeCustomSelectList = () => {
-  isCustomSelectListOpen = false;
+const SelectItem = (item: string) => {
+  selectedItems.value.push(item);
+  availableItems.value = availableItems.value.filter((i) => i !== item);
+  isCustomSelectListOpen.value = false; //Close Modal
+};
+
+const RemoveItem = (item: string) => {
+  selectedItems.value = selectedItems.value.filter((i) => i !== item);
+  availableItems.value.push(item);
 };
 </script>
 
@@ -36,11 +48,26 @@ const closeCustomSelectList = () => {
           <font-awesome-icon :icon="faSortDown" :class="$style['select_ic']" />
           <div :class="$style['custom-select-list']" v-show="isCustomSelectListOpen">
             <ul>
-              <li @click="closeCustomSelectList">Tin tức</li>
-              <li @click="closeCustomSelectList">Hoạt động</li>
-              <li @click="closeCustomSelectList">Hội thảo</li>
-              <li @click="closeCustomSelectList">Phỏng vấn</li>
+              <li
+                v-for="(item, index) in availableItems"
+                :key="index"
+                @click.stop="SelectItem(item)"
+              >
+                {{ item }}
+              </li>
             </ul>
+          </div>
+        </div>
+        <div :class="$style['news__list-active']">
+          <div
+            :class="$style['news__list-item']"
+            v-for="(item, index) in selectedItems"
+            :key="index"
+          >
+            {{ item }}
+            <div :class="$style['news__item-cancel']" @click.stop="RemoveItem(item)">
+              <font-awesome-icon :icon="faXmark" :class="$style['cancel_ic']" />
+            </div>
           </div>
         </div>
       </div>
