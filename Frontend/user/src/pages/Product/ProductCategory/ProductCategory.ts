423e6ec89dcee2f1e64783ae894179d4a7fa61ba
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2023-06-21 01:13:15 +0700

    TV:update Pagination, category

---
+++ Frontend/user/src/pages/Product/ProductCategory/ProductCategory.ts
@@ -0,0 +1,95 @@
+export const category = [
+  {
+    title: 'Vật liệu chỉnh nha ABC',
+    data: [
+      {
+        name: 'Lò xo chỉnh nha'
+      },
+      {
+        name: 'Kềm chỉnh nha'
+      },
+      {
+        name: 'Thun chỉnh chỉnh nha'
+      },
+      {
+        name: 'Chỉ chỉnh nha'
+      },
+      {
+        name: 'Kẹp gấp mắc cài'
+      }
+    ]
+  },
+  {
+    title: 'Vật liệu chỉnh nha ADC',
+    data: [
+      {
+        name: 'Lò xo chỉnh nha'
+      },
+      {
+        name: 'Kềm chỉnh nha'
+      },
+      {
+        name: 'Thun chỉnh chỉnh nha'
+      },
+      {
+        name: 'Chỉ chỉnh nha'
+      },
+      {
+        name: 'Kẹp gấp mắc cài'
+      }
+    ]
+  },
+  {
+    title: 'Vật liệu chỉnh nha XYZ',
+    data: [
+      {
+        name: 'Lò xo chỉnh nha'
+      },
+      {
+        name: 'Kềm chỉnh nha'
+      },
+      {
+        name: 'Thun chỉnh chỉnh nha'
+      },
+      {
+        name: 'Chỉ chỉnh nha'
+      },
+      {
+        name: 'Kẹp gấp mắc cài'
+      }
+    ]
+  }
+];
+
+// export const categorys = [
+//   {
+//     title: 'Vật liệu chỉnh nha ABC',
+//     data: [
+//       'Lò xo chỉnh nha',
+//       'Kềm chỉnh nha',
+//       'Thun chỉnh chỉnh nha',
+//       'Chỉ chỉnh nha',
+//       'Kẹp gấp mắc cài'
+//     ]
+//   },
+//   {
+//     title: 'Vật liệu chỉnh nha ADC',
+//     data: [
+//       'Lò xo chỉnh nha',
+//       'Kềm chỉnh nha',
+//       'Thun chỉnh chỉnh nha',
+//       'Chỉ chỉnh nha',
+//       'Kẹp gấp mắc cài'
+//     ]
+//   },
+//   {
+//     title: 'Vật liệu chỉnh nha XYZ',
+//     data: [
+//       'Lò xo chỉnh nha',
+//       'Kềm chỉnh nha',
+//       'Thun chỉnh chỉnh nha',
+//       'Chỉ chỉnh nha',
+//       'Kẹp gấp mắc cài'
+//     ]
+//   }
+// ];
