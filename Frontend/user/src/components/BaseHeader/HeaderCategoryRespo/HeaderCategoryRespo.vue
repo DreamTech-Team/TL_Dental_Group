ee25a52a3b576b1bd2797a12c67a6fbff1b5311f
trankhuong <tranduykhuongit@gmail.com>
2023-07-04 22:04:13 +0700

    DK: header responsive

--- Frontend/user/src/components/BaseHeader/HeaderCategoryRespo/HeaderCategoryRespo.vue
+++ Frontend/user/src/components/BaseHeader/HeaderCategoryRespo/HeaderCategoryRespo.vue
@@ -2,58 +2,66 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { RouterLink } from 'vue-router';
 import { ref } from 'vue';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { categories } from '../HeaderHandle';
 
-const cate1Hover = ref('');
+const props = defineProps({
+  pageMobile: String,
+  currentItem: String,
+  hiddenNav: Function
+});
+
+const subActive = ref('none');
 </script>
 <template>
   <div>
-    <ul :class="$style['hder-category']">
-      <li :class="$style['hder-category__line']"></li>
-      <li
-        v-for="item1 in categories.slice(0, 6)"
-        v-on:mouseenter="cate1Hover = item1.slug"
-        v-on:mouseleave="cate1Hover = ''"
-        :key="item1.slug"
-        :class="$style['hder-category__item']"
-      >
-        <router-link to="" :class="$style['hder-category__item-link']">
-          <p>{{ item1.name }}</p>
-          <font-awesome-icon :icon="faChevronRight" size="xs" />
+    <ul
+      :class="[
+        $style['hder-category'],
+        $style[
+          props.pageMobile === 'sanpham' && props.currentItem === 'sanpham'
+            ? 'hder-category--show'
+            : 'hder-category--hidden'
+        ]
+      ]"
+    >
+      <li v-for="item in categories" :key="item.slug" :class="$style['hder-category__item']">
+        <router-link
+          to=""
+          :class="$style['hder-category__item-link']"
+          v-on:click="subActive = subActive === item.slug ? 'none' : item.slug"
+        >
+          <p>{{ item.name }}</p>
+          <font-awesome-icon :icon="faChevronDown" size="xs" />
         </router-link>
 
-        <!-- <ul
+        <ul
           :class="[
             $style['hder-category'],
-            $style['hder-category-sub'],
-            $style[cate1Hover === item1.slug ? 'hder-category-show' : 'hder-category--hidden']
+            $style['hder-category__sub'],
+            $style[subActive === item.slug ? 'hder-category--show' : 'hder-category--hidden']
           ]"
-          :style="{ maxHeight: heightCate2(item1?.slug) + 'vh' }"
         >
-          <li :class="$style['hder-category__line']"></li>
           <li
-            v-for="item2 in item1.list.slice(0, 8)"
-            :key="item2.slug"
+            v-for="subItem in item.list.slice(0, 4)"
+            :key="subItem.slug"
             :class="$style['hder-category__item']"
           >
-            <router-link to="" :class="$style['hder-category__item-link']">
-              <p>{{ item2.name }}</p>
+            <router-link to="" :class="$style['hder-category__item-link']" @click="hiddenNav">
+              <p>{{ subItem.name }}</p>
             </router-link>
           </li>
 
           <li :class="$style['hder-category__more']" key="xemtatca">
-            <router-link to="/sanpham" :class="$style['hder-category__more-link']">
-              Xem tất cả
+            <router-link
+              to="/sanpham"
+              :class="$style['hder-category__more-link']"
+              @click="hiddenNav"
+            >
+              Xem tất cả ...
             </router-link>
           </li>
-        </ul> -->
-      </li>
-
-      <li :class="$style['hder-category__more']" key="xemtatca">
-        <router-link to="/sanpham" :class="$style['hder-category__more-link']">
-          Xem tất cả
-        </router-link>
+        </ul>
       </li>
     </ul>
   </div>
