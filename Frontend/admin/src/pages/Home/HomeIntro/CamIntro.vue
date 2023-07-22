7733fe943d9a9bd1164ac245601d8d1505ea0307
Andy Koi <anhkhoi1192002@gmail.com>
2023-07-23 00:57:22 +0700

    Khoi: Finish Crop Image

--- Frontend/admin/src/pages/Home/HomeIntro/CamIntro.vue
+++ Frontend/admin/src/pages/Home/HomeIntro/CamIntro.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch, type Ref } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import Swal from 'sweetalert2';
 import CamBtn from '@/components/ImageBtn/ImageBtn.vue';
+import CropImage from '@/components/CropImage/CropImage.vue';
 import useAxios, { type DataResponse } from '@/hooks/useAxios';
-
-interface MyFile extends File {
-  name: string;
-}
 
 const context = defineProps({
   uuid: {
@@ -35,26 +32,34 @@ const emits = defineEmits<{
   // eslint-disable-next-line no-unused-vars
   (e: 'update-content', data: { image: string }): void;
 }>();
-const selectedImage = ref(context.image);
-const fileInput = ref<HTMLInputElement | null>(null);
-const file = ref<MyFile | null>(null);
+
+const urlFile: Ref<string | null> = ref(context.image);
+const isCrop = ref(false);
+const isOpenInput = ref(false);
+const fileData = ref();
+const finalImage = ref();
 
 //Open file image
 const openFileInput = () => {
-  fileInput.value?.click();
+  isOpenInput.value = !isOpenInput.value;
 };
 
-//Choose image
-const handleFileInputChange = (event: Event) => {
-  const inputElement = event.target as HTMLInputElement;
-  file.value = inputElement.files?.[0] || null;
+const base64ToBlob = (base64Data: string) => {
+  const byteString = atob(base64Data.split(',')[1]);
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: 'image/png' });
+};
 
-  if (file.value) {
-    const reader = new FileReader();
-    reader.onload = () => {
-      selectedImage.value = reader.result as string;
-    };
-    reader.readAsDataURL(file.value);
+const handleCroppedImage = (result: string) => {
+  if (result) {
+    urlFile.value = result;
+
+    fileData.value = base64ToBlob(result);
+    finalImage.value = new File([fileData.value], 'image.png', { type: 'image/png' });
   }
 };
 
@@ -69,7 +74,7 @@ const submitForm = () => {
 
   const formData = new FormData();
   formData.append('data', JSON.stringify(object));
-  formData.append('image', file.value as Blob);
+  formData.append('image', finalImage.value as Blob);
   const { response } = useAxios<DataResponse>(
     'patch',
     '/home/section3',
@@ -93,7 +98,7 @@ const submitForm = () => {
         if (result.isConfirmed) {
           Swal.close();
           emits('update-content', {
-            image: selectedImage.value
+            image: urlFile.value || ''
           });
           emits('close');
         }
@@ -116,14 +121,8 @@ const submitForm = () => {
       </div>
       <div :class="$style.camintro__modal__body">
         <div :class="$style.camintro__ctn">
-          <img :src="selectedImage" alt="CEO" />
+          <img v-if="urlFile" :src="urlFile" alt="CEO" />
           <div :class="$style.camintro__image_overlay">
-            <input
-              type="file"
-              ref="fileInput"
-              style="display: none"
-              @change="handleFileInputChange"
-            />
             <CamBtn @click="openFileInput" />
           </div>
         </div>
@@ -134,6 +133,17 @@ const submitForm = () => {
       </div>
     </div>
   </div>
+  <crop-image
+    :heightCrop="570"
+    :widthCrop="420"
+    :heightWrap="570"
+    :widthWrap="420"
+    :check="isOpenInput"
+    v-show="isCrop"
+    @close="isCrop = false"
+    @open="isCrop = true"
+    @crop="handleCroppedImage"
+  />
 </template>
 
 <style module scoped lang="scss">
