2828bf1b85c2ab6ceddeff291fe315a34dc5585c
joseph <tranduykhuongit@gmail.com>
2023-09-02 20:49:54 +0700

    DK: fix create News

--- Frontend/admin/src/pages/Activity/ActivityTable/ModalActivity/UpdateActivity.vue
+++ Frontend/admin/src/pages/Activity/ActivityTable/ModalActivity/UpdateActivity.vue
@@ -201,8 +201,12 @@ const example_image_upload_handler = (blobInfo: BlobInfo, progress: ProgressFunc
   new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
-    xhr.open('POST', 'https://dry-ants-production.up.railway.app/api/v1/fileUpload');
-
+    xhr.open('POST', import.meta.env.VITE_API_ENDPOINT + '/fileUpload');
+    const getInforAdmin = localStorage.getItem('infor_admin');
+    if (getInforAdmin) {
+      const inforAdmin = JSON.parse(getInforAdmin);
+      xhr.setRequestHeader('Authorization', 'Bearer ' + inforAdmin.token);
+    }
     xhr.upload.onprogress = (e) => {
       progress((e.loaded / e.total) * 100);
     };
