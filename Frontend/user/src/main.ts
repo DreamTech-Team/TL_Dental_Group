c84ab5f63751325a85dcdc63319c695a4581a669
NG-MinG <wgmin.it@outlook.com>
2023-09-20 16:04:34 +0700

    setup google analystic

--- Frontend/user/src/main.ts
+++ Frontend/user/src/main.ts
@@ -2,12 +2,23 @@ import './styles/main.scss';
 // import ElPagination from 'element-plus';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import VueGtag from 'vue-gtag';
 import { createMetaManager } from 'vue-meta';
 
 import App from './App.vue';
 import router from './router';
 
-const app = createApp(App).use(createMetaManager()).use(createPinia()).use(router);
+const app = createApp(App)
+  .use(createMetaManager())
+  .use(createPinia())
+  .use(VueGtag, {
+    appName: 'TLDentalCustomer',
+    pageTrackerScreenviewEnabled: true,
+    config: { id: import.meta.env.MEASUREMENT_ID }
+  })
+  .use(router);
 
 await router.isReady();
 app.mount('#app');
+
+// https://blog.stackademic.com/how-to-use-google-analytics-in-your-vue-3-options-api-with-vue-gtag-f7e217a8fa56
