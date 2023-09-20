06c52efab5f77daa89f97f752efe1ad0bf9887fa
NG-MinG <wgmin.it@outlook.com>
2023-09-20 14:39:54 +0700

    setup vue meta

--- Frontend/user/src/main.ts
+++ Frontend/user/src/main.ts
@@ -2,13 +2,12 @@ import './styles/main.scss';
 // import ElPagination from 'element-plus';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createMetaManager } from 'vue-meta';
 
 import App from './App.vue';
 import router from './router';
 
-const app = createApp(App);
-// app.use(ElPagination);
-app.use(createPinia());
-app.use(router);
+const app = createApp(App).use(createMetaManager()).use(createPinia()).use(router);
 
+await router.isReady();
 app.mount('#app');
