1465dca9ec5df20cce78ddc4d95703f1b59ebc91
Bao <20127557@student.hcmus.edu.vn>
2023-11-01 01:30:07 +0700

    BL: refactor work env

--- Frontend/user/src/pages/Recruitment/RecruitmentPage.vue
+++ Frontend/user/src/pages/Recruitment/RecruitmentPage.vue
@@ -27,7 +27,7 @@ import useAxios, { type DataResponse } from '@/hooks/useAxios';
 // import RecruitmentPoster from './RecruitmentPoster/RecruitmentPoster.vue';
 // import RecruitmentVision from './RecruitmentVision/RecruitmentVision.vue';
 // import RecruitmentValue from './RecruitmentValue/RecruitmentValue.vue';
-// import RecruitmentEnviroment from './RecruitmentEnviroment/RecruitmentEnviroment.vue';
+import RecruitmentEnviroment from './RecruitmentEnviroment/RecruitmentEnviroment.vue';
 // import RecruitmentNavScroll from './RecruitmentNavScroll/RecruitmentNavScroll.vue';
 // import RecruitmentWork from './RecruitmentWork/RecruitmentWork.vue';
 
@@ -441,60 +441,7 @@ onMounted(() => {
         </div>
       </div>
     </div>
-    <div :class="$style.container__envir">
-      <div :class="$style['container__envir-title']">Môi Trường Làm Việc Chuyên Nghiệp</div>
-      <div :class="$style['container__envir-block']">
-        <div
-          :class="[
-            $style['container__envir-block-img1'],
-            $style['container__envir-block-img1-bottom']
-          ]"
-        >
-          <img :src="img1" alt="none" />
-        </div>
-        <div :class="$style['container__envir-block-img2']">
-          <img :src="img2" alt="none" />
-          <div
-            :class="[
-              $style['container__envir-block-img2-circle'],
-              $style['container__envir-block-img2-circle-1']
-            ]"
-          ></div>
-        </div>
-        <div
-          :class="[
-            $style['container__envir-block-img1'],
-            $style['container__envir-block-img1-center']
-          ]"
-        >
-          <img :src="img3" alt="none" />
-        </div>
-        <div
-          :class="[
-            $style['container__envir-block-img2'],
-            $style['container__envir-block-img2-bottom']
-          ]"
-        >
-          <div>
-            <div
-              :class="[
-                $style['container__envir-block-img2-circle'],
-                $style['container__envir-block-img2-circle-2']
-              ]"
-            ></div>
-            <img :src="img4" alt="none" />
-          </div>
-        </div>
-        <div
-          :class="[
-            $style['container__envir-block-img1'],
-            $style['container__envir-block-img1-top']
-          ]"
-        >
-          <img :src="img5" alt="none" />
-        </div>
-      </div>
-    </div>
+    <recruitment-enviroment />
     <div :class="$style.container__recruit">
       <div
         :class="[
