c756280d5bddd4c07401b5221ea4aa9e9fde1b51
Andy Koi <anhkhoi1192002@gmail.com>
2023-10-31 09:53:06 +0700

    Khoi: refactoring news detail p2

--- Frontend/user/src/pages/NewsDetail/NewsDetailContent/NewsDetailContent.vue
+++ Frontend/user/src/pages/NewsDetail/NewsDetailContent/NewsDetailContent.vue
@@ -3,7 +3,7 @@ import { onMounted, ref, watch } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import useAxios, { type DataResponse } from '@/hooks/useAxios';
-import dayjs from 'dayjs';
+import { handleFormatTime } from '@/utils/handleFornatTime';
 import 'dayjs/locale/vi';
 import Time from '@/assets/imgs/NewsDetail/Time.png';
 
@@ -64,15 +64,6 @@ watch(getNews.response, () => {
     contentButtonRight.value = dataNews.value[indexNews.value + 1].title;
   }
 });
-
-// Format lại thời gian
-const handleFormatTime = (time: string) => {
-  const inputDate = dayjs(time).locale('vi'); // Đặt ngôn ngữ
-
-  const daysOfWeek = ['Chủ Nhật', 'Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm', 'Thứ Sáu', 'Thứ Bảy'];
-
-  return `${daysOfWeek[inputDate.day()]}, ${inputDate.format('DD/MM/YYYY, HH:mm [GMT]Z')}`;
-};
 
 // Xử lí chuyển bài viết bên trái
 const handleClickLeft = () => {
